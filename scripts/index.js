b3166186fa802b358105e16ea146fafa76a7297f
Nick Koltcov <nickkoltcov@yandex.ru>
2024-09-09 13:37:05 +0300

    первая отправка работы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+function createCard(cards, deletecard) {
+    const cardTemplate = document.querySelector('#card-template').content.cloneNode(true);
+    const cardElement = cardTemplate.querySelector('.card');
 
-// @todo: Функция создания карточки
+    const imageElement = cardElement.querySelector('.card__image');
+    const titleElement = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+    imageElement.src = cards.link;
+    imageElement.alt = cards.name;
+    titleElement.textContent = cards.name;
 
-// @todo: Вывести карточки на страницу
+    deleteButton.addEventListener('click', () => {
+        deletecard(cardElement)
+    });
+    
+    return cardElement;
+}
+
+function renderCards(cards) {
+    const placesList = document.querySelector('.places__list');
+  
+    cards.forEach(cardData => {
+        const card = createCard(cardData, deleteCard);
+        placesList.appendChild(card);
+    });
+}
+
+function deleteCard(cardElement) {
+    const placesList = document.querySelector('.places__list');
+    placesList.removeChild(cardElement);
+}
+
+renderCards(initialCards);
